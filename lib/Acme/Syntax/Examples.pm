b346f647b5882f9841fffd3b4be67812bcfdc1ab
Randy Stauner <randy@magnificent-tears.com>
2015-03-31 20:39:05 -0700

    initial commit

---
+++ lib/Acme/Syntax/Examples.pm
@@ -0,0 +1,15 @@
+# vim: set ts=2 sts=2 sw=2 expandtab smarttab:
+use strict;
+use warnings;
+
+package Acme::Syntax::Examples;
+
+# ABSTRACT: undef
+
+1;
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
